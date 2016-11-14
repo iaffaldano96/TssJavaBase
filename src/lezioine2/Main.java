203979dd67ffc0577e67c0b3095113c928aca4a1
tss <tss@debian-tss24>
2016-11-14 12:33:05 +0100

    Creatp package lezione 2

--- src/lezioine2/Main.java
+++ src/lezioine2/Main.java
@@ -11,13 +11,37 @@ package lezioine2;
  */
 public class Main {
 
+    //variabili ==> campi o proprietà o attributi
+    //statica
+    private static int colore; 
+    //d'istanza
+    private int carattere;
+    
     /**
      * Da questo metodo partirà l'esecuzione del pregramma
      * @param args 
      */
     public static void main(String[] args) {
+        
         System.out.println("Start esempio commenti..");
-              
-    }
-    
+        
+        //variabili locali
+        String frase;
+        int totNumeroUtenti=10;
+        if(totNumeroUtenti>0){
+            int x = 5;
+        }
+        else{
+            int x=6;
+        }
+        
+        // if ternario
+        frase = (totNumeroUtenti>0) ? "Tota positivo" : "Tot negativo";
+        
+        int x=4;
+        
+        System.out.println("Totale: "+totNumeroUtenti);
+        System.out.println(frase);
+
+    }    
 }
